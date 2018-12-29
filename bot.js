4797edb56069fd6cf91e05f9676b6081848cd418
zenilepl <45181384+zenilepl@users.noreply.github.com>
2018-12-29 22:47:58 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,7 +3,7 @@ const client = new Discord.Client();
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
-client.user.setGame(`$تقديم`,"http://twitch.tv/S-F")
+client.user.setGame(`$active`,"http://twitch.tv/S-F")
   console.log('')
   console.log('made by NoHaxJustZenile')
   console.log('╔[═════════════════════════════════════════════════════════════════]╗')
